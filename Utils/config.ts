ff04432eda5cc84ac10898bc8b3e8b69f3e5d0c6
vovaberdi <vova.berdibek@gmail.com>
2023-01-07 17:59:03 +0200

    sec commit heroku

--- Utils/config.ts
+++ Utils/config.ts
@@ -3,11 +3,18 @@
 class Config { 
     public port = 3001; 
     // mysql database
-    public mySQLhost = "localhost";
-    public mySQLUser = "root";
-    public mySQLPassword = "12345678";
-    public mySQLdb = "vication";
+    public mySQLhost = "eu-cdbr-west-03.cleardb.net";
+    public mySQLUser = "bfc584f0ddae85";
+    public mySQLPassword = "c1b3e516";
+    public mySQLdb = "heroku_9580274f3f074b6";
 }
 
 const config = new Config();
 export default config
+
+
+
+// public mySQLhost = "localhost";
+// public mySQLUser = "root";
+// public mySQLPassword = "12345678";
+// public mySQLdb = "vication";
